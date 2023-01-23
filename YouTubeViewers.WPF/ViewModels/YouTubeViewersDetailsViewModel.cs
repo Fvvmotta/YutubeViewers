5e6e41401520171142f5905907ea38669a540c53
Fernando Motta <fvvmotta@gmail.com>
2023-01-23 10:56:52 -0300

    Added views and viewmodels to project

---
+++ YouTubeViewers.WPF/ViewModels/YouTubeViewersDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeViewers.WPF.ViewModels
+{
+    public class YouTubeViewersDetailsViewModel : ViewModelBase
+    {
+        public string UserName { get; }
+        public string IsSubscribedDisplay { get; }
+        public string IsMemberDisplay { get; }
+
+        public YouTubeViewersDetailsViewModel()
+        {
+            UserName = "SingletonSean";
+            IsSubscribedDisplay = "Yes";
+            IsMemberDisplay = "No";
+        }
+    }
+}
---
+++ YouTubeViewers.WPF/ViewModels/YouTubeViewersListingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeViewers.WPF.ViewModels
+{
+    public class YouTubeViewersListingViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<YouTubeViewersListingItemViewModel> _youTubeViewersListingItemViewModel;
+
+        public IEnumerable<YouTubeViewersListingItemViewModel> YouTubeViewersListingItemViewModel => _youTubeViewersListingItemViewModel;
+
+        public YouTubeViewersListingViewModel()
+        {
+            _youTubeViewersListingItemViewModel = new ObservableCollection<YouTubeViewersListingItemViewModel>();
+
+            _youTubeViewersListingItemViewModel.Add(new YouTubeViewersListingItemViewModel("Mary"));
+            _youTubeViewersListingItemViewModel.Add(new YouTubeViewersListingItemViewModel("Sean"));
+            _youTubeViewersListingItemViewModel.Add(new YouTubeViewersListingItemViewModel("Alan"));
+        }
+    }
+}
